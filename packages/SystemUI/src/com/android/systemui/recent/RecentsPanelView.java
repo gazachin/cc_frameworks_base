ac1379dd6bc55279336fcd0b86e1b7012db5692b
Nelson <ngiordano75@gmail.com>
2012-10-13 21:06:04 -0400

    Add kill-all to Recents

--- packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
+++ packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
@@ -57,6 +57,7 @@ import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
@@ -100,6 +101,8 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
     ImageView mPlaceholderThumbnail;
     View mTransitionBg;
     boolean mHideRecentsAfterThumbnailScaleUpStarted;
+
+    private Button mRecentsKillAllButton;
 
     Handler mTaskHandler;
 
@@ -578,6 +581,23 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
             }
         }
 
+        boolean recent_kill_all_button = Settings.System.getInt(mContext.getContentResolver(),
+                      Settings.System.RECENT_KILL_ALL_BUTTON, 0) == 1;
+
+        mRecentsKillAllButton = (Button) findViewById(R.id.recents_kill_all_button);
+        if (mRecentsKillAllButton != null){
+            if (recent_kill_all_button){ //set the listener
+                mRecentsKillAllButton.setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        killAllRecentApps();
+                    }
+                });
+            } else { // hide the button completely (gone)
+                mRecentsKillAllButton.setVisibility(View.GONE);
+            }
+        }
+
         mPreloadTasksRunnable = new Runnable() {
             public void run() {
                 // If we set our visibility to INVISIBLE here, we avoid an extra call to
@@ -1010,6 +1030,23 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
             }
         });
         popup.show();
+     }
+
+    private void killAllRecentApps(){
+        final ActivityManager am = (ActivityManager)
+                mContext.getSystemService(Context.ACTIVITY_SERVICE);
+        if(!mRecentTaskDescriptions.isEmpty()){
+            for(TaskDescription ad : mRecentTaskDescriptions){
+                am.removeTask(ad.persistentTaskId, ActivityManager.REMOVE_TASK_KILL_PROCESS);
+                // Accessibility feedback
+                setContentDescription(
+                        mContext.getString(R.string.accessibility_recents_item_dismissed, ad.getLabel()));
+                sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_SELECTED);
+                setContentDescription(null);
+            }
+            mRecentTaskDescriptions.clear();
+        }
+        hide(false);
     }
 
     public void addContainer(View container){
